a14a11b9d1e72758790171e1d16f54604c2a9aa9
dino <dino@330z.net>
2018-05-13 21:04:47 +1000

    Emit generic events
    Add dependence events

--- lib/src/TeleDart.dart
+++ lib/src/TeleDart.dart
@@ -1,46 +1,72 @@
 import 'dart:async';
 import 'dart:io';
+import 'package:events/events.dart';
 import 'package:TeleDart/src/Telegram/Telegram.dart';
 import 'package:TeleDart/src/Telegram/Model.dart';
 
-class TeleDart{
+class TeleDart extends Events{
   Telegram _tg;
 
   bool _webhook = false;
   final MAX_TIMEOUT = 60;
 
-  TeleDart(String token){
+  TeleDart(String token) : super(){
     _tg = new Telegram(token);
   }
 
   void startPolling(
-      {int offset = 0, int limit = 100, timeout = 30,
-        List<String> allowed_updates}) {
-    if(!_webhook)
-      _tg.getUpdates(offset: offset, limit: limit,
-          timeout: timeout, allowed_updates: allowed_updates)
-          .then((updates) {
-            if(updates.length > 0){
-              for (Update update in updates) {
-                // TODO: add updates to event queue
-                print(update.update_id);
-                print(update.message.text);
-                offset = update.update_id + 1;
-              }
+      {int offset: 0, int limit: 100, timeout: 30,
+        List<String> allowed_updates}){
+
+    if(_webhook)
+      throw new Exception('TeleDart Error: Webhook is enabled.');
+    if(timeout > MAX_TIMEOUT)
+      throw new Exception('TeleDart Error: Timeout may not greater than 60.');
+
+    _tg.getUpdates(offset: offset, limit: limit,
+        timeout: timeout, allowed_updates: allowed_updates)
+        .then((updates) {
+          if(updates.length > 0){
+            for (Update update in updates) {
+              updatesHandler(update);
+              offset = update.update_id + 1;
             }
+          }
+          startPolling(offset: offset, limit: limit,
+              timeout: timeout, allowed_updates: allowed_updates);
+        })
+        // TODO: find out what exceptions can be ignored
+        .catchError((error) {
+          if(error is HandshakeException)
             startPolling(offset: offset, limit: limit,
                 timeout: timeout, allowed_updates: allowed_updates);
-          })
-          // TODO: find out what exception is normal
-          .catchError((error) {
-            if(error is HandshakeException)
-              startPolling(offset: offset, limit: limit,
-                  timeout: timeout, allowed_updates: allowed_updates);
-            else
-              print(error.toString());
-          });
-    else
-      throw new Exception('TeleDart Error: Webhook is enabled.');
+          else
+            throw new Exception(error.toString());
+        });
   }
 
+  // TODO: add updates to event queue
+  void updatesHandler(Update update){
+    if(update.message != null){
+      this.emit('message', update.message);
+    }
+    else if(update.edited_messaged != null){
+      this.emit('edited_messaged', update.edited_messaged);
+    }
+    else if(update.channel_post != null){
+      this.emit('channel_post', update.channel_post);
+    }
+    else if(update.inline_query != null){
+      this.emit('inline_query', update.inline_query);
+    }
+    else if(update.chosen_inline_result != null){
+      this.emit('chosen_inline_result', update.chosen_inline_result);
+    }
+    else if(update.callback_query != null){
+      this.emit('callback_query', update.callback_query);
+    }
+    else if(update.pre_checkout_query != null){
+      this.emit('callback_query', update.callback_query);
+    }
+  }
 }
