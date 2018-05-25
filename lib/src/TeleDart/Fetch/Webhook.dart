d819535299fe64cb8ececd9b6a80c56b3979b9c9
dino <dino@330z.net>
2018-05-25 22:30:24 +1000

    cleanup
    Add exceptions

--- lib/src/TeleDart/Fetch/Webhook.dart
+++ lib/src/TeleDart/Fetch/Webhook.dart
@@ -29,23 +29,29 @@ class Webhook {
   Webhook(this.telegram, this.url, this.secretPath,
       {this.port: 443, this.privateKey, this.certificate,
         this.max_connections: 40, this.allowed_updates}) {
+
+    if([443, 80, 88, 8443].indexOf(this.port) < 0)
+      throw new WebhookException('Ports currently supported for Webhooks: 443, 80, 88, 8443.');
+    if(max_connections > 100 || max_connections < 1)
+      throw new WebhookException('Limit must between 1 and 100.');
+
     _updateStreamController = new StreamController();
+
     // prefix url and secret path
     if(this.url.endsWith('\/'))
       this.url.substring(0, this.url.length -1);
     if(!this.secretPath.startsWith('\/'))
       this.secretPath = '\/' + this.secretPath;
 
-  }
-
-  void setWebhook() {
-
+    // serup SecurityContext
     if(privateKey != null && certificate != null) {
       _context = new io.SecurityContext();
       _context.usePrivateKeyBytes(privateKey.readAsBytesSync());
       _context.useCertificateChainBytes(certificate.readAsBytesSync());
     }
+  }
 
+  void setWebhook() {
     // initialise server
     Future serverFuture = _context == null ?
     io.HttpServer.bind(io.InternetAddress.LOOPBACK_IP_V4.address, port) :
@@ -63,12 +69,13 @@ class Webhook {
   }
 
   void startWebhook() {
+    if(_server == null)
+      throw new WebhookException('Please use setWebhook to initialise webhook before start webhook.');
     _server.listen((io.HttpRequest request) {
       if(request.method == 'POST' &&
           request.uri.path == this.secretPath ) {
-        request.transform(UTF8.decoder).join().then((data) {
-          emitUpdate(_dson.map(JSON.decode(data), new Update(), true));
-        });
+        request.transform(UTF8.decoder).join().then((data) =>
+            emitUpdate(_dson.map(JSON.decode(data), new Update(), true)));
         request.response.write('ok');
         request.response.close();
       }
@@ -77,7 +84,9 @@ class Webhook {
         ..statusCode = io.HttpStatus.METHOD_NOT_ALLOWED;
       }
       request.response.close();
-    });
+    })
+        .onError((error) =>
+    new Future.error(new WebhookException(error.toString())));
   }
 
   Future deleteWebhook() async {
@@ -86,8 +95,10 @@ class Webhook {
             new Future.error(new WebhookException(error.toString())));
   }
 
-  void stopWebhook() =>
+  void stopWebhook() {
+    if(_server != null)
       _server.close();
+  }
 
   void emitUpdate(Update update) =>
       _updateStreamController.add(update);
