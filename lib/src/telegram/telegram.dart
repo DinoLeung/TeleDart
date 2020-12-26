b8077b5e40ed0e571249fe2cf5aa6b8d59eaa518
DinoLeung <dino.ph.leung@gmail.com>
2020-12-26 16:05:54 +1030

    added local telegram server support

--- lib/src/telegram/telegram.dart
+++ lib/src/telegram/telegram.dart
@@ -130,6 +130,27 @@ class Telegram {
   Future<User> getMe() async =>
       User.fromJson(await HttpClient.httpGet('${_baseUrl}${_token}/getMe'));
 
+  /// Use this method to log out from the cloud Bot API server before launching the bot locally.
+  /// You must log out the bot before running it locally,
+  /// otherwise there is no guarantee that the bot will receive updates.
+  /// After a successful call, you can immediately log in on a local server,
+  /// but will not be able to log in back to the cloud Bot API server for 10 minutes.
+  /// Returns True on success. Requires no parameters.
+  ///
+  /// https://core.telegram.org/bots/api#logout
+  Future<bool> logOut() async =>
+      await HttpClient.httpGet('${_baseUrl}${_token}/logOut');
+
+  /// Use this method to close the bot instance before moving it from one local server to another.
+  /// You need to delete the webhook before calling this method to ensure that the bot isn't
+  /// launched again after server restart.
+  /// The method will return error 429 in the first 10 minutes after the bot is launched.
+  /// Returns True on success. Requires no parameters.
+  /// 
+  /// https://core.telegram.org/bots/api#close
+  Future<bool> close() async =>
+      await HttpClient.httpGet('${_baseUrl}${_token}/close');
+
   /// Use this method to send text messages. On success, the sent [Message] is returned.
   ///
   /// [**Formatting options**](https://core.telegram.org/bots/api#formatting-options)
