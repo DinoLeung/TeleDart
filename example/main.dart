4e760cd7512ad939dcce550cbef4d470288169d8
DinoLeung <dino.ph.leung@gmail.com>
2021-07-08 20:43:41 +0930

    updated example

--- example/main.dart
+++ example/main.dart
@@ -1,5 +1,5 @@
 import 'dart:io' show Platform;
-import 'dart:io' as io;
+// import 'dart:io' as io;
 
 import 'package:teledart/teledart.dart';
 import 'package:teledart/telegram.dart';
@@ -44,14 +44,14 @@ Future<void> main() async {
 
   // You can even filter streams with stream processing methods
   // See: https://www.dartlang.org/tutorials/language/streams#methods-that-modify-a-stream
-  // teledart
-  //     .onMessage(keyword: 'dart')
-  //     .where((message) => message.text.contains('telegram'))
-  //     .listen((message) => teledart.replyPhoto(
-  //         message,
-  //         //  io.File('example/dash_paper_plane.png'),
-  //         'https://raw.githubusercontent.com/DinoLeung/TeleDart/master/example/dash_paper_plane.png',
-  //         caption: 'This is how Dash found the paper plane'));
+  teledart
+      .onMessage(keyword: 'dart')
+      .where((message) => message.text?.contains('telegram') ?? false)
+      .listen((message) => teledart.replyPhoto(
+          message,
+          //  io.File('example/dash_paper_plane.png'),
+          'https://raw.githubusercontent.com/DinoLeung/TeleDart/master/example/dash_paper_plane.png',
+          caption: 'This is how Dash found the paper plane'));
 
   // Sick of boilerplates? Reply messages like below, nice and tidy
   // Short hands also available for answer query methods
@@ -60,18 +60,16 @@ Future<void> main() async {
       .listen((message) => message.reply('Stand with Hong Kong'));
 
   // Inline mode.
-  // teledart.onInlineQuery().listen((inlineQuery) => inlineQuery.answer([
-  //       InlineQueryResultArticle()
-  //         ..id = 'ping'
-  //         ..title = 'ping'
-  //         ..input_message_content = (InputTextMessageContent()
-  //           ..message_text = '*pong*'
-  //           ..parse_mode = 'MarkdownV2'),
-  //       InlineQueryResultArticle()
-  //         ..id = 'ding'
-  //         ..title = 'ding'
-  //         ..input_message_content = (InputTextMessageContent()
-  //           ..message_text = '_dong_'
-  //           ..parse_mode = 'MarkdownV2')
-  //     ]));
+  teledart.onInlineQuery().listen((inlineQuery) => inlineQuery.answer([
+        InlineQueryResultArticle(
+            id: 'ping',
+            title: 'ping',
+            input_message_content: InputTextMessageContent(
+                message_text: '*pong*', parse_mode: 'MarkdownV2')),
+        InlineQueryResultArticle(
+            id: 'ding',
+            title: 'ding',
+            input_message_content: InputTextMessageContent(
+                message_text: '*_dong_*', parse_mode: 'MarkdownV2')),
+      ]));
 }
